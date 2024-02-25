7bf38737c71088db80eb86e9bf680d1affb0f704
Roberto Viola <Cagnulein@gmail.com>
2024-02-25 15:42:25 +0100

    Initial Commit

---
+++ ClimbTracker Watch App/ClimbTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbTrackerApp.swift
+//  ClimbTracker Watch App
+//
+//  Created by Roberto Viola on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimbTracker_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimbTracker/ClimbTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimbTrackerApp.swift
+//  ClimbTracker
+//
+//  Created by Roberto Viola on 25/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ClimbTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
